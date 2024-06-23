7b3a9cf1be13d8689070b04b5a13d5b44f12db2d
Alexandr0208 <al.tolki@yandex.ru>
2024-06-23 21:05:24 +0300

    Add files via upload

--- mobule_2_8.py
+++ mobule_2_8.py
@@ -1,11 +1,12 @@
-def get_multiplied_digits(*number):
-    first = int(number[0])
-    if not number:
-        return 0
-    if len(number) == 1:
+def get_multiplied_digits(number):
+    str_number = str(number)
+    first = int(str_number[0])
+    if len(str_number) == 1:
         return first
-    else:
+    if len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
+
+
 
 str_number = '40203'
 print(get_multiplied_digits(str_number))
